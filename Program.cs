f78131e33a40f2e6c3f86a3899121047fd724848
Beatrix Devanti <beatrix.devanti.ft21@mail.umy.ac.id>
2022-10-21 17:03:20 +0700

    add void

--- Program.cs
+++ Program.cs
@@ -12,7 +12,23 @@ namespace QuickSort
         private int cmp_count = 0; // number of comparasion
         private int mov_count = 0; // number of data movements
 
-   
+        //  Number of elements in array
+        private int n;
+
+
+
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray cah have maximum 20 elements \n");
+
 
 
 
@@ -30,4 +46,6 @@ namespace QuickSort
 
             }
         }
-   
+
+    }
+}
