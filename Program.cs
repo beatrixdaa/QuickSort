885e3d7c3c6d76fb588a67fb45124ab1212ab205
Beatrix Devanti <beatrix.devanti.ft21@mail.umy.ac.id>
2022-10-21 17:40:57 +0700

    sort the list right pivot

--- Program.cs
+++ Program.cs
@@ -101,9 +101,11 @@ namespace QuickSort
             //sort the list on the left of pivot using quick sort
             q_sort(low, j - 1);
 
-            
-
+            //sort the list on the right of pivot using quick sort
+            q_sort(j + 1, high);
         }
+
+
 
     }
 }
