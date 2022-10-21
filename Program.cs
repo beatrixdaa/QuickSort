0e699f5e6715f1b6c9f03cb85b09da94c66fd30f
Beatrix Devanti <beatrix.devanti.ft21@mail.umy.ac.id>
2022-10-21 16:56:27 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
+        //  Number of elements in array
+        private int n;
+
+        void input()
+        {
+            while (true)
+            {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+
+}
